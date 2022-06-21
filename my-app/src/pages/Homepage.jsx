22771b5ff41d53cbcf1539f695d9726f517919b4
greenT-Hee <heehee970322@gmail.com>
2022-06-21 21:54:25 +0900

    banner_dummy

--- my-app/src/pages/Homepage.jsx
+++ my-app/src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import Header from "../Components/Header/Header"
 import Banner from "../Components/Banner/Banner"
 import Main from "../Components/Main/Main"
 import Footer from "../Components/Footer/Footer"
+// import { useEffect } from "react"
 
 export default function Homepage() {
   return (
