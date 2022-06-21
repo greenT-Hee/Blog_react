1d5527ea1183b814f017facc933ac50c140dd64e
greenT-Hee <heehee970322@gmail.com>
2022-06-21 22:03:40 +0900

    json_updage

--- my-app/src/Components/Posts/Posts.jsx
+++ my-app/src/Components/Posts/Posts.jsx
@@ -1,24 +1,24 @@
 import "./posts.css"
 import Post from "../Post/Post"
 import { useEffect , useState  } from "react"
-import { Data } from "../../data"
+// import { Data } from "../../data"
 
 export default function Posts() {
-  // const [dummy, setDummy] = useState([])
+  const [dummy, setDummy] = useState([])
 
-  // useEffect(()=>{
-  //   fetch("https://greent-hee.github.io/Blog_react/my-app/public/data.json")
-  //   .then(res => {
-  //     return res.json()
-  //   })
-  //   .then(data => {
-  //    setDummy(data)
-  //   })
-  // },[])
+  useEffect(()=>{
+    fetch("https://greent-hee.github.io/Blog_react/my-app/public/data.json")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+     setDummy(data.posts)
+    })
+  },[])
     return (
       <ul className="posts">
         {
-          Data.posts.map(item => {
+          dummy.map(item => {
           return <Post 
             key = {item.id}
             thumbnailSrc = {item.thumbnail}
