5704bd4dbe420924086245d0c422edfb321e28a8
greenT-Hee <heehee970322@gmail.com>
2022-06-21 22:51:23 +0900

    About_dummy_setting

--- my-app/src/Components/Categories/Categories.jsx
+++ my-app/src/Components/Categories/Categories.jsx
@@ -1,15 +1,24 @@
 import './categories.css'
+import { useState, useEffect } from 'react'
 
-export default function Categories(props) {
+export default function Categories() {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("https://greent-hee.github.io/Blog_react/my-app/public/data.json")
+    .then(res=> {
+      return res.json()
+    })
+    .then(data=> {
+      // console.log(data.users[0].category)
+      setCategories(data.users[0].category)
+    })
+  })
   return (
     <>
-      <li><a href="#">Style</a></li>
-      <li><a href="#">Life</a></li>
-      <li><a href="#">Tech</a></li>
-      <li><a href="#">Sports</a></li>
-      <li><a href="#">Photo</a></li>
-      <li><a href="#">Develop</a></li>
-      <li><a href="#">Music</a></li>
+      {categories.map((item) => {
+        return <li><a href="#">{item}</a></li>
+      })}
     </>
   )
 }
