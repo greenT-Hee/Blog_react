e93304f6ad2ef4cfab9288c348994b3bda1470dd
greenT-Hee <heehee970322@gmail.com>
2022-06-27 23:12:53 +0900

    Category_data_map

--- my-app/src/Components/PostArticle/PostArticle.jsx
+++ my-app/src/Components/PostArticle/PostArticle.jsx
@@ -2,35 +2,29 @@ import './postArticle.css'
 import Contents from './Contents/Contents'
 import Author from './Author/Author'
 import Title from './Title/Title'
-import Category from '../PostArticle/Category/Category'
+import Category from '../Category/Category'
 import Buttons from './Buttons/Buttons'
 // import { useEffect, useState } from 'react'
 
-export default function PostArticle({category}) {
+export default function PostArticle(props) {
+    console.log("postArticle",props.post)
     return (
     <div className='view'>
         <div className='max-width'>
             <section className='wrap-box'>
                 <div className='inner'>
                     <Author />
-                    <Category category={category}/>
+                    {props.post.map((item,index)=> {
+                        return <Category category={item.category} key={item.id}/>
+                    })
+                    }
+                    <Category category={props.post}/>
                     <Title />
                     <Contents />
                     <Buttons />
-                    {/* <div className='btn-group'>
-                        <a href="#" className='btn-modify'>
-                            <span className='a11y-hidden'>modify</span>
-                        </a>
-                        <button className='btn-delete'>
-                            <span className='a11y-hidden'>delete</span>
-                        </button>
-                    </div>
-                    <Link to ="/" className='btn-back'>
-                        <span className='a11y-hidden'>back</span>
-                    </Link> */}
                 </div>
             </section>
         </div>
     </div>
-  )
+)
 }
